5d87388ac7e06949f9f72029d3fc7dff67963309
Nfx1z <mahadikanafiz86@gmail.com>
2025-10-23 14:04:08 +0700

    Add: model settings

---
+++ app/Models/Setting.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
+
+class Setting extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'key',
+        'value',
+        'type',
+        'group',
+    ];
+
+    public static function get($key, $default = null)
+    {
+        return Cache::remember("setting_{$key}", 3600, function () use ($key, $default) {
+            $setting = self::where('key', $key)->first();
+            return $setting ? $setting->value : $default;
+        });
+    }
+
+    public static function set($key, $value)
+    {
+        $setting = self::updateOrCreate(
+            ['key' => $key],
+            ['value' => $value]
+        );
+        
+        Cache::forget("setting_{$key}");
+        
+        return $setting;
+    }
+
+    public static function getGroup($group)
+    {
+        return self::where('group', $group)->get()->pluck('value', 'key')->toArray();
+    }
+}
